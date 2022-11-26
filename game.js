88d1202c039d317bf2a5c959b1016ff66f9c8a5d
pushkevich <91333311+pushkevich@users.noreply.github.com>
2022-11-26 12:13:57 +0500

    updt

--- game.js
+++ game.js
@@ -1,41 +1,45 @@
-const GAME = [];
+const readline = require('readline');
+let GAME = [];
 class Game {
-    constructor(name,health,damage,speed) {
+    constructor(number,name,health,damage,speed) {
+        this.number=number
         this.name=name;
         this.health=health;
         this.damage=damage;
         this.speed=speed;
     }
 
-    hit(health,speed){
+    hit(){
 
     };
-    counterstrike(health,speed){
+    counterstrike(){
 
     };
-    get army(){
-        const army = new Map()
-
-    }
 }
 const shooter = new Game(
+    1,
     'стрелок',
     150,
     5,
     30);
 const swordsman = new Game(
+    2,
     'мечник',
     250,
     12,
     12);
 const spearman = new Game(
+    3,
     'копейщик',
     250,
     12,
     12);
 const BOSS = new Game(
+    4,
     'BIG RUSSIAN BOSS',
     1000,
     15,
     20,
 )
+const win = new Game()
+console.log(BOSS.hit)
