c777673007e38ceb604d9d5f86ac0ea801ae7e2c
pushkevich <91333311+pushkevich@users.noreply.github.com>
2022-12-03 10:57:33 +0500

    updt

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
-const readline = require('readline');
+const readlinePromises = require('node:readline/promises');
+const rl = readlinePromises.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 let GAME = [];
 class Game {
     constructor(number,name,health,damage,speed) {
@@ -8,11 +12,28 @@ class Game {
         this.damage=damage;
         this.speed=speed;
     }
-
+    choice_an_army(){
+        console.log(shooter,swordsman,spearman)
+        rl.question('выберите воинов(number and quantity separated by commas)', (number,quantity) =>{
+            let numb = shooter.number || swordsman.number || spearman.number;
+            let quant = [];
+            if(number === Number(numb) && quantity === Number(quant)){
+                GAME.push(numb * quant)
+                console.log(GAME)
+            }
+        })
+    }
+    choice_an_action(){
+        rl.question('хотите ударить?(y | n)', answer =>{
+            if(answer === 'y'){
+                win.hit()
+            }
+        })
+    };
     hit(){
 
     };
-    counterstrike(){
+    counter_strike(){
 
     };
 }
@@ -21,25 +42,25 @@ const shooter = new Game(
     'стрелок',
     150,
     5,
-    30);
+    30)
 const swordsman = new Game(
     2,
     'мечник',
     250,
     12,
-    12);
+    12)
 const spearman = new Game(
     3,
     'копейщик',
     250,
     12,
-    12);
+    12)
 const BOSS = new Game(
     4,
     'BIG RUSSIAN BOSS',
     1000,
     15,
-    20,
+    20
 )
 const win = new Game()
-console.log(BOSS.hit)
+console.log(win.choice_an_army())
